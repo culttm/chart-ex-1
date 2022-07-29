5f65c030c796679b14cc1bf08372bf10ab89f26e
Dmitry Kozlov <culttm@gmail.com>
2022-07-29 17:00:23 +0300

    lol

--- src/charts/data.ts
+++ src/charts/data.ts
@@ -1,32 +1,32 @@
 export const Data = () => [
-    {
-        date: new Date(),
-        value: 10.5
-    },
-    {
-        date: new Date(),
-        value: 11
-    },
-    {
-        date: new Date(),
-        value: 11
-    },
-    {
-        date: new Date(),
-        value: 11
-    },
-    {
-        date: new Date(),
-        value: 11
-    },
-    {
-        date: new Date(),
-        value: 1.3
-    },
-    {
-        date: new Date(),
-        value: 244
-    },
+    // {
+    //     date: new Date(),
+    //     value: 10.5
+    // },
+    // {
+    //     date: new Date(),
+    //     value: 11
+    // },
+    // {
+    //     date: new Date(),
+    //     value: 11
+    // },
+    // {
+    //     date: new Date(),
+    //     value: 11
+    // },
+    // {
+    //     date: new Date(),
+    //     value: 11
+    // },
+    // {
+    //     date: new Date(),
+    //     value: 1.3
+    // },
+    // {
+    //     date: new Date(),
+    //     value: 244
+    // },
     {
         date: new Date(),
         value: 11.3
@@ -43,156 +43,156 @@ export const Data = () => [
         date: new Date(),
         value: 11
     },
-    // {
-    //     date: new Date(),
-    //     value: 10
-    // },
-    // {
-    //     date: new Date(),
-    //     value: 10
-    // },
-    // {
-    //     date: new Date(),
-    //     value: 9
-    // },
-    // {
-    //     date: new Date(),
-    //     value: 10
-    // },
-    // {
-    //     date: new Date(),
-    //     value: 10
-    // },
-    // {
-    //     date: new Date(),
-    //     value: 10
-    // },
-    // {
-    //     date: new Date(),
-    //     value: 10
-    // },
-    // {
-    //     date: new Date(),
-    //     value: 224
-    // },
-    // {
-    //     date: new Date(),
-    //     value: 224
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 500) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 200) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: 224
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 500) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 200) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 500) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // },
-    // {
-    //     date: new Date(),
-    //     value: ~~(Math.random() * 100) + 2
-    // }
+    {
+        date: new Date(),
+        value: 10
+    },
+    {
+        date: new Date(),
+        value: 10
+    },
+    {
+        date: new Date(),
+        value: 9
+    },
+    {
+        date: new Date(),
+        value: 10
+    },
+    {
+        date: new Date(),
+        value: 10
+    },
+    {
+        date: new Date(),
+        value: 10
+    },
+    {
+        date: new Date(),
+        value: 10
+    },
+    {
+        date: new Date(),
+        value: 224
+    },
+    {
+        date: new Date(),
+        value: 224
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 500) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 200) + 2
+    },
+    {
+        date: new Date(),
+        value: 224
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 500) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 200) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 500) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    },
+    {
+        date: new Date(),
+        value: ~~(Math.random() * 100) + 2
+    }
 ]
